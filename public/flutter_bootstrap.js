d101dca1df490c70b1d44de3ba0a601c6069505b
shivam-dubey-byte <dubeyshivamraj@gmail.com>
2025-06-06 02:16:03 +0530

    0.1.1

--- public/flutter_bootstrap.js
+++ public/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3565322617"
+    serviceWorkerVersion: "2309569950"
   }
 });
